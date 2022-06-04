aa1f2ba101a7ba35ad70b7fefbe80b257e33daa0
Rabiaparlak <68352029+Rabiaparlak@users.noreply.github.com>
2022-06-04 19:08:23 +0300

    Add files via upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            UserSeeder::class,
+            QuizSeeder::class,
+        ]);
+//seederler çağrılır.
+    }
+}
